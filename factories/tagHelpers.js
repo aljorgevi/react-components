a794900345d53ee1e3597b61b659a81b6adee51a
Jorge Mena <jormencar@yahoo.com>
2024-04-10 23:15:59 +0100

    Add TagManager class and related methods in tagHelpers.js

--- factories/tagHelpers.js
+++ factories/tagHelpers.js
@@ -30,3 +30,90 @@ export function extractTagDetails(tag) {
 		objectId
 	}
 }
+
+class TagManager {
+	/**
+	 * Creates a TagManager instance with initial definitions and attributes.
+	 * @param {Object} initialDefinitions - Initial definitions for the tag manager.
+	 * @param {Object} initialAttributes - Initial set of attributes.
+	 */
+	constructor(initialDefinitions = {}, initialAttributes = {}) {
+		this.definitions = {
+			type: 'job',
+			...initialDefinitions
+		}
+		this.attributes = initialAttributes
+	}
+
+	/**
+	 * Returns the TagManager instance.
+	 * @return {TagManager} The current instance of TagManager.
+	 */
+	getTag() {
+		return this
+	}
+
+	/**
+	 * Adds or updates a definition by key.
+	 * @param {string} key - The key for the definition to add or update.
+	 * @param {Object} detail - The detail of the definition to add or update.
+	 */
+	setDefinition(key, detail) {
+		this.definitions[key] = detail
+	}
+
+	/**
+	 * Returns the definitions object.
+	 * @return {Object} The current definitions object.
+	 */
+	getDefinitions() {
+		return this.definitions
+	}
+
+	/**
+	 * Returns the attributes object.
+	 * @return {Object} The current attributes object.
+	 */
+	getAttributes() {
+		return this.attributes
+	}
+
+	/**
+	 * Adds or updates an attribute by name.
+	 * @param {string} name - The name of the attribute.
+	 * @param {Object} detail - The details of the attribute.
+	 */
+	setAttribute(name, detail) {
+		this.attributes[name] = detail
+	}
+
+	/**
+	 * Adds or updates multiple attributes at once.
+	 * @param {Object} attributes - An object containing multiple attributes to be added or updated.
+	 */
+	setAttributes(attributes) {
+		for (const [key, value] of Object.entries(attributes)) {
+			this.attributes[key] = value
+		}
+	}
+}
+
+const initialAttributes = {
+	flowName: { type: 'STRING', value: 'Initial Flow Name' }
+}
+
+const jobToRun = new TagManager({}, initialAttributes)
+
+// Adding a new attribute
+jobToRun.setAttribute('description', {
+	type: 'STRING',
+	id: 'description'
+})
+
+// Adding multiple attributes at once
+jobToRun.setAttributes({
+	priority: { type: 'INTEGER', value: 1 },
+	status: { type: 'STRING', value: 'Active' }
+})
+
+console.log(jobToRun.getTag())
